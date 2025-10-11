c4d537ab70a3ff04ec250a36b037be95d7db5c60
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-10-11 14:43:20 +0300

    simplified telegram_bot even further

--- Scripts_LLM_trader/telegram_bot/telegram_bot.py
+++ Scripts_LLM_trader/telegram_bot/telegram_bot.py
@@ -1,30 +1,28 @@
 import requests
 import os
+import json
 
 
-def __get_from_json(key): 
+def _get_from_json(key): 
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, "credentials.json")
 
     with open(file_path, "r") as f:
-        f = f.read()
-        import json
-
-        credentials = json.loads(f)
+        credentials = json.load(f)
         return credentials[key]
     return None
 
 
-def __send_message(chat_id, text): # send one message to chat with chat_id
-    url = f"https://api.telegram.org/bot{__get_from_json('telegram_bot_token')}/sendMessage"
+def _send_message(chat_id, text): # send one message to chat with chat_id
+    url = f"https://api.telegram.org/bot{_get_from_json('telegram_bot_token')}/sendMessage"
     res = requests.post(url, json={"chat_id": chat_id, "text": text})
     res.raise_for_status()
 
 
 def notify_listeners(message): # send a message to all listeners
-    for listener in __get_from_json("listeners"):
-        __send_message(chat_id=listener, text=message)
+    for listener in _get_from_json("listeners"):
+        _send_message(chat_id=listener, text=message)
 
 
 if __name__ == "__main__":
-    print(__get_from_json("telegram_bot_token"))
+    print("telegram_bot_token: ", _get_from_json("telegram_bot_token"))
