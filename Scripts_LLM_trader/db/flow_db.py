06bfa29f01b5ff0601e7311809341d507e98b165
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-10-18 04:22:36 +0300

    minor changes

---
+++ Scripts_LLM_trader/db/flow_db.py
@@ -0,0 +1,4 @@
+
+# id, ticker, date, all_llm_responses, last_llm_response, final_order_decision, order_amount , t_open, t_high, t_low, t_close, profit_made,
+
+# market kapandıktan sonra bu script tekrardan çalışıp open, high low, close priceları dolduracak ve profit_made ı hesaplayacak
--- Scripts_LLM_trader/main.py
+++ Scripts_LLM_trader/main.py
@@ -2,6 +2,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 from flows.trading_flow_v2 import flow_v2 
 from data_gathering.FinnHub import save_news
+import datetime
 
 import sys, os # To make it work no matter where it is executed from
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -10,9 +11,9 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 if __name__ == "__main__":
     logging.info("Starting flow_v2")
     save_news()
-
-
-    flow_v2()
+    
+    date=datetime.date.today()
+    # flow_v2(date=datetime.date.today(), notify_users=True)
     """resp=poc_flow("2025-10-16")
     for r in resp:
         print(r)"""
