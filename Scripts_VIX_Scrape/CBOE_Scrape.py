3100377c7427d5bb03ff7be25e8657fe2bee9475
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-10-25 05:58:12 +0300

    minor fixes, ready to test

--- Scripts_VIX_Scrape/CBOE_Scrape.py
+++ Scripts_VIX_Scrape/CBOE_Scrape.py
@@ -1,4 +1,8 @@
-""" Created on 07-21-2024 23:07:40 @author: DenizYalimYilmaz """
+"""Created on 07-21-2024 23:07:40 @author: DenizYalimYilmaz"""
+
+from fatwoman_log_setup import script_end_log
+from fatwoman_dir_setup import CBOE_Scrape_Data_File, CBOE_Scrape_timestamp_format
+
 import logging
 from datetime import datetime as dt
 from selenium import webdriver
@@ -9,83 +13,85 @@ import os
 import time
 import pandas as pd
 
-# TIMESTAMP_FORMAT = CBOE_Scrape_timestamp_format
-HOUR_FORMAT = '%H:%M'
+TIMESTAMP_FORMAT = CBOE_Scrape_timestamp_format
+HOUR_FORMAT = "%H:%M"
 MAX_ATTEMPTS = 1
-LINK = 'https://www.cboe.com/tradable_products/vix/vix_futures/'
+LINK = "https://www.cboe.com/tradable_products/vix/vix_futures/"
 
-def _clean_table_and_save_to_csv(table_2d): 
+
+def _clean_table_and_save_to_csv(table_2d):
     logging.info("Atttempting to manip the table scrapped at csv_manip")
     # Clean data
     columns = ["Maturity", "Last", "Change", "High", "Low", "Settlement", "Volume"]
     df_futures = pd.DataFrame(table_2d[1:], columns=columns)
-    df_futures.iloc[0,0] = 'VIX'
-    df_futures['Volume'].replace(',','', regex=True, inplace=True)
-    df_futures['Volume'] = pd.to_numeric(df_futures['Volume'], errors='coerce')
-    df_futures['Settlement'].replace('-','', regex=True, inplace=True)
-    df_futures['Settlement'] = pd.to_numeric(df_futures['Settlement'], errors='coerce')
-    # TODO: df_futures['Timestamp'] = dt.now().strftime(TIMESTAMP_FORMAT)
-    df_futures['Hour']      = dt.now().strftime(HOUR_FORMAT)
+    df_futures.iloc[0, 0] = "VIX"
+    df_futures["Volume"].replace(",", "", regex=True, inplace=True)
+    df_futures["Volume"] = pd.to_numeric(df_futures["Volume"], errors="coerce")
+    df_futures["Settlement"].replace("-", "", regex=True, inplace=True)
+    df_futures["Settlement"] = pd.to_numeric(df_futures["Settlement"], errors="coerce")
+    df_futures["Timestamp"] = dt.now().strftime(TIMESTAMP_FORMAT)
+    df_futures["Hour"] = dt.now().strftime(HOUR_FORMAT)
     # data includes vix itself so maturity formatting is not valid
-    # df_futures['Maturity']      = dt.now().strftime(hour_format)
+    # df_futures["Maturity"] = dt.now().strftime(HOUR_FORMAT)
     # df_futures['Maturity'] = pd.to_datetime(df_futures['Maturity'], format='%m/%d/%Y') #CBOE_RAW_timestamp_format = '%m/%d/%Y'
 
+    # Write file Add header if file does not exist
     try:
-        # Write file Add header if file does not exist
-        # TODO file_exists = os.path.exists(CBOE_Scrape_Data_File)
-        # df_futures.to_csv(CBOE_Scrape_Data_File, mode='a', sep=',', header=not file_exists, index=False)
+        file_exists = os.path.exists(CBOE_Scrape_Data_File)
+        df_futures.to_csv(
+            CBOE_Scrape_Data_File,
+            mode="a",
+            sep=",",
+            header=not file_exists,
+            index=False,
+        )
         pass
     except Exception as e:
         logging.error("error while writing to csv: \n", e)
-    # logging.info("Successfully cleaned data gathered and saved to ", CBOE_Scrape_Data_File)
+    logging.info("Successfully cleaned data gathered and saved to ", CBOE_Scrape_Data_File)
     return True
 
-if __name__ == "__main__":
-    logging.info("Starting CBOE Data Scrape "+ time.strftime("%Y-%m-%d %H:%M:%S"))
 
-    for _attempt in range(MAX_ATTEMPTS): 
+if __name__ == "__main__":
+    logging.info("Starting CBOE Data Scrape " + time.strftime("%Y-%m-%d %H:%M:%S"))
+    
+    for _attempt in range(MAX_ATTEMPTS):
         logging.info("setting up the driver")
         options = webdriver.FirefoxOptions()
-        options.add_argument("--headless") 
+        options.add_argument("--headless")
         success = False
         with webdriver.Firefox(options=options) as driver:
-                
-            driver.get(LINK); 
-            logging.info('Driver get done; driver setup complete, initiating scraping')
+
+            driver.get(LINK)
+            logging.info("Driver get done; driver setup complete, initiating scraping")
 
             try:
-                table_2d = [] 
-                table_element = driver.find_element(By.TAG_NAME, "table") 
+                table_2d = []
+                table_element = driver.find_element(By.TAG_NAME, "table")
                 for row in table_element.find_elements(By.TAG_NAME, "tr"):
                     table_row = []
-                    values = [col.text for col in row.find_elements(By.TAG_NAME, "td")] 
+                    values = [col.text for col in row.find_elements(By.TAG_NAME, "td")]
                     table_2d.append(values)
 
-                print("size: ", len(table_2d))
-                # Drop the first values(symbols) by transposing and dropping first row and transposing back
+                # drop first column of each row                
                 for row in table_2d:
                     if row:
                         row.pop(0)
 
-
-                print("size: ", len(table_2d))
-                for val in table_2d:
-                    print(val)
-                print()
-
-                if _clean_table_and_save_to_csv(table_2d=table_2d):
+                if _clean_table_and_save_to_csv(table_2d=table_2d): # clean data and save to csv
                     success = True
- 
 
             except ValueError as ve:
-                print(f"Attempt {_attempt + 1} failed with ValueError: {ve}. Retrying...")
-                logging.error(f"Attempt {_attempt + 1} failed with ValueError: {ve}. Retrying...")
+                logging.error(
+                    f"Attempt {_attempt + 1} failed with ValueError: {ve}. Retrying..."
+                )
                 time.sleep(5)
 
             except Exception as e:
-                print(f"Attempt {_attempt + 1} failed with error: {e}. Retrying...")
-                logging.error(f"Attempt {_attempt + 1} failed with     error: {ve}. Retrying...")
-                
+                logging.error(
+                    f"Attempt {_attempt + 1} failed with error: {e}. Retrying..."
+                )
+
                 time.sleep(5)
 
         if success:
@@ -93,17 +99,17 @@ if __name__ == "__main__":
 
     # TODO: script_end_log()
 
-        # options.add_argument("-profile")
-        # options.add_argument(firefox_profile1)
+    # options.add_argument("-profile")
+    # options.add_argument(firefox_profile1)
 
 # import ace_tools as tools; tools.display_dataframe_to_user(name="Stock Data", dataframe=df)
 
 
-        # options = Options()  #webdriver.ChromeOptions()
-        # # options.add_argument("--headless")
-        # driver = webdriver.Chrome(options=options)
-        # driver.get('https://www.cboe.com/tradable_products/vix/vix_futures/')
-        # source = driver.page_source
+# options = Options()  #webdriver.ChromeOptions()
+# # options.add_argument("--headless")
+# driver = webdriver.Chrome(options=options)
+# driver.get('https://www.cboe.com/tradable_products/vix/vix_futures/')
+# source = driver.page_source
 
 
 # df_temp = df0['timestamp'][:5391]
@@ -219,4 +225,3 @@ script_end_log()
 
 
 """
-
