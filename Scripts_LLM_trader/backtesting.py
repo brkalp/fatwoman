e1ca4a344064e30062aefcb7d11500362a6383ef
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-11-01 04:17:03 +0300

    minor name change

--- Scripts_LLM_trader/backtesting.py
+++ Scripts_LLM_trader/backtesting.py
@@ -14,16 +14,16 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
 def test_flow_v1(start_date:str, end_date:str, ticker:str): # flow 1 # BU DA THREADLENEBİLİR
-    start = datetime.strptime(start_date, "%Y-%m-%d")
-    end = datetime.strptime(end_date, "%Y-%m-%d")
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
 
-    current = start
-    while current <= end:
-        date_cur = current.strftime("%Y-%m-%d")
-        logging.info(f"Running flow for: {date_cur}")
-        flow_v1(ticker=ticker, date=date_cur, notify_users=False, headline_factory="ticker_news")
+    cur_date = start_date
+    while cur_date <= end_date:
+        date_str = cur_date.strftime("%Y-%m-%d")
+        logging.info(f"Running flow for: {date_str}")
+        flow_v1(ticker=ticker, date=date_str, notify_users=False, headline_factory="ticker_news")
 
-        current += timedelta(days=1)
+        cur_date += timedelta(days=1)
 
 """
 def test_for_days(start_date:str, end_date:str): # flow 2
